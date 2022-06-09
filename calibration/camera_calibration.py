c50a2f5b5033d275165e111581c2678158e421bd
Omar Alam <omeralam7@gmail.com>
2022-06-09 11:22:26 -0400

    camera calibration chessboard

---
+++ calibration/camera_calibration.py
@@ -0,0 +1,62 @@
+"""
+Code taken from : https://nikatsanka.github.io/camera-calibration-using-opencv-and-python.html
+
+Learn more about camera calibration: https://docs.opencv.org/4.x/dc/dbb/tutorial_py_calibration.html
+"""
+
+
+import numpy as np
+import cv2
+import glob
+import yaml
+#import pathlib
+
+# termination criteria
+criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+objp = np.zeros((7*7,3), np.float32)
+objp[:,:2] = np.mgrid[0:7,0:7].T.reshape(-1,2)
+# Arrays to store object points and image points from all the images.
+obj_points = [] # 3d point in real world space
+img_points = [] # 2d points in image plane.
+
+images = glob.glob(r'../calibration_images/*.jpg')
+
+found = 0
+for fname in images:  # Here, 10 can be changed to whatever number you like to choose
+    img = cv2.imread(fname) # Capture frame-by-frame
+    #print(images[im_i])
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # Find the chess board corners
+    ret, corners = cv2.findChessboardCorners(gray, (7,7), None)
+    # If found, add object points, image points (after refining them)
+    if ret == True:
+        obj_points.append(objp)   # Certainly, every loop objp is the same, in 3D.
+        corners2 = cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
+        img_points.append(corners2)
+        # Draw and display the corners
+        img = cv2.drawChessboardCorners(img, (7,7), corners2, ret)
+        found += 1
+        cv2.imshow('img', img)
+        cv2.waitKey(500)
+        # if you want to save images with detected corners
+        # uncomment following 2 lines and lines 5, 18 and 19
+        # image_name = path + '/calibresult' + str(found) + '.png'
+        # cv2.imwrite(image_name, img)
+
+print("Number of images used for calibration: ", found)
+
+# When everything done, release the capture
+# cap.release()
+cv2.destroyAllWindows()
+
+# calibration
+ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(obj_points, img_points, gray.shape[::-1], None, None)
+
+# transform the matrix and distortion coefficients to writable lists
+data = {'camera_matrix': np.asarray(mtx).tolist(),
+        'dist_coeff': np.asarray(dist).tolist()}
+
+# and save it to a file
+with open("calibration_matrix.yaml", "w") as f:
+    yaml.dump(data, f)
