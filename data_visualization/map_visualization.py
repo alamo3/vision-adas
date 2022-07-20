dfef689813844919928fd0a7f5ffd910f241a2c3
Omar Alam <omeralam7@gmail.com>
2022-07-20 23:08:35 +0500

    Fix tile server not loading

--- data_visualization/map_visualization.py
+++ data_visualization/map_visualization.py
@@ -20,9 +20,10 @@ class DataMapVisualization(customtkinter.CTk):
         self.frame_disp = customtkinter.CTkFrame(master=self, width=DataMapVisualization.WIDTH, corner_radius=0)
         self.frame_disp.grid(row=0, column=0, padx=0, pady=0, sticky="nsew")
 
-        self.map_widget = tkintermapview.TkinterMapView(self.frame_disp, width=800, height=600, corner_radius=0)
+        self.map_widget = tkintermapview.TkinterMapView(self.frame_disp, width=DataMapVisualization.WIDTH, height=DataMapVisualization.HEIGHT, corner_radius=0)
+        self.map_widget.set_tile_server("https://mt0.google.com/vt/lyrs=m&hl=en&x={x}&y={y}&z={z}&s=Ga", max_zoom=22)
         self.map_widget.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
-        self.map_widget.set_address("Mississauga, Ontario")
+        self.map_widget.set_address("1276 Vanrose Street Mississauga")
 
 
 if __name__ == "__main__":
